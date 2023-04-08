67682258ee09f2fcabf1b154f48b6177090df5e6
Your Name <you@example.com>
2023-04-08 14:38:56 +0530

    Inbox

---
+++ src/Components/Inbox.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Button, Container, Form, Nav, Navbar } from 'react-bootstrap'
+const Inbox = () => {
+  return (
+    <div>
+         <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">React mail</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+       
+           
+           
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
+
+export default Inbox
