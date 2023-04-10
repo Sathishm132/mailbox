212e69444df86b02c420f1a4514dd974eb964cd2
Your Name <you@example.com>
2023-04-10 12:09:35 +0530

    read mail

--- src/Components/Inbox.js
+++ src/Components/Inbox.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./Inbox.css"
 import {Button, Container, Form, Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
 const Inbox = () => {
+  const[mail ,setMail]=useState([])
+  useEffect(()=>{
+    const fetch=async()=>{
+      const response= await axios.get("https://crud-12e65-default-rtdb.asia-southeast1.firebasedatabase.app/mail.json")
+    let fetchdata=[]
+    for(let key in response.data){
+       fetchdata.push({...response.data[key],id:key})
+  
+       }
+       console.log(fetchdata)
+       setMail(fetchdata)
+    }
+    fetch()
+    
+    
+    
+  },[])
+
+const inboxmail=mail.map((data)=>(<ul><div dangerouslySetInnerHTML={{ __html: data.content }} /></ul>))
+ 
   return (
     <div>
-         <Navbar bg="light" expand="lg">
+         <Navbar bg="dark" expand="lg">
       <Container>
         <Navbar.Brand href="#">React mail</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
@@ -23,7 +47,26 @@ const Inbox = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    <div className='d-flex justify-content-inline'>
+    <div className='conatiner-fluid'>
+       
+       <div className='row'>
+        <div className='bg-light col-auto col-md- min-vh-100'>
+            <a href='/mail' className='nav-link'>compose</a>
+          
+        </div>
+       
+       
+       </div>
     </div>
+    <div >
+        {inboxmail}
+        {}
+          
+        </div>
+     </div>
+     </div>
+    
   )
 }
 
--- src/Components/Mailpage.js
+++ src/Components/Mailpage.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Button, Container, Form, InputGroup } from 'react-bootstrap'
 import { Editor } from "react-draft-wysiwyg";
-import { EditorState } from 'draft-js'
+import { convertToRaw, EditorState } from 'draft-js'
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
+import draftToHtml from "draftjs-to-html"
+import axios from 'axios';
 
 const Mailpage = () => {
   let editorState = EditorState.createEmpty();
   const [description, setDescription] = useState(editorState);
   const onEditorStateChange = (editorState) => {
     setDescription(editorState);
+  }
+  const email=useRef();
+  const title=useRef()
+  const clickhandler=(e)=>{
+    e.preventDefault();
+    const draft={
+      mail:email.current.value,
+      title:title.current.value,
+      content:draftToHtml(convertToRaw(description.getCurrentContent()))
+    }
+    console.log(draft)
+    axios.put("https://crud-12e65-default-rtdb.asia-southeast1.firebasedatabase.app/mail.json",draft)
   }
   return (
     <div>
@@ -17,6 +31,7 @@ const Mailpage = () => {
       <InputGroup size="sm" className="mb-3 mt-5">
         <InputGroup.Text id="inputGroup-sizing-sm">To</InputGroup.Text>
         <Form.Control
+         ref={email}
           aria-label="To"
           aria-describedby="inputGroup-sizing-sm"
         />
@@ -24,24 +39,24 @@ const Mailpage = () => {
       <InputGroup size="sm" className="mb-3 ">
         <InputGroup.Text id="inputGroup-sizing-sm">subject</InputGroup.Text>
         <Form.Control
+        ref={title}
           aria-label="To"
           aria-describedby="inputGroup-sizing-sm"
         />
       </InputGroup>
       <Editor
                   editorState={description}
-                 
+
                   wrapperClassName="wrapperClassName"
                   editorClassName="editorClassName"
                    toolbarClassName="toolbarClassName"
                   onEditorStateChange={ onEditorStateChange}
                 />
-                <Button>send</Button>
+                <Button onClick={clickhandler}>send</Button>
           </Form>                           
       </Container>
-     
+
     </div>
   )
 }
-
-export default Mailpage
+ export default Mailpage
